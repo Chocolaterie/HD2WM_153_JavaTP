49c4f275c47fb9fd2adf20adbd3d63063601b48d
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2024-08-20 14:56:44 +0200

    First commit

---
+++ src/main/java/eni/tp/app/eni_app/EniAppApplication.java
@@ -0,0 +1,13 @@
+package eni.tp.app.eni_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EniAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EniAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eni/tp/app/eni_app/EniAppApplicationTests.java
@@ -0,0 +1,13 @@
+package eni.tp.app.eni_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EniAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
