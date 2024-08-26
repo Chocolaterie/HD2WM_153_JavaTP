6eb5f72a542df616d6f25bd32f4009d731decbd6
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2024-08-26 16:13:18 +0200

    Correction

--- src/main/java/eni/tp/app/eni_app/AppController.java
+++ src/main/java/eni/tp/app/eni_app/AppController.java
@@ -7,11 +7,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@SessionAttributes({"loggedUser"})
 @Controller
 public class AppController {
 
--- src/main/java/eni/tp/app/eni_app/AuthController.java
+++ src/main/java/eni/tp/app/eni_app/AuthController.java
@@ -4,12 +4,23 @@ import eni.tp.app.eni_app.bo.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+@SessionAttributes({"loggedUser"})
 @Controller
 public class AuthController {
 
     @GetMapping("login")
     public String showLogin(Model model) {
+        // Tester si deja connecté (le user "loggedUser" en session n'est pas null) retourner page d'erreur
+        Member loggedUser = (Member) model.getAttribute("loggedUser");
+
+        if (loggedUser != null){
+            return "auth/already-logged-page";
+        }
+
         // Instancier un User vide (email et password vide)
         Member user = new Member();
 
@@ -18,5 +29,14 @@ public class AuthController {
 
         // Afficher la page qui contient le formulaire
         return "auth/login-page";
+    }
+
+    @PostMapping("login")
+    public String processLogin(@ModelAttribute Member user, Model model) {
+        // Mettre l'user dans la session
+        model.addAttribute("loggedUser", user);
+
+        // rediriger sur ta page d'accueil
+        return "redirect:/";
     }
 }
